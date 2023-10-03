0fa9ded0e5626ccf90e163ed18801f761cdb0689
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-10-03 22:11:26 +0500

    Update the Layout with Routing
    Updated App with Universal layout and Added routing for user page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,35 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import NavBar from '@/Components/NavBar';
+import SmUserCard from '@/Components/UserCard/SmUserCard';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Tweetify | Home',
-  description: 'Tweetify is a open social media platform for public to share there opinions.',
-}
+	title: 'Tweetify | Home',
+	description:
+		'Tweetify is a open social media platform for public to share there opinions.',
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="flex justify-center">{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className='flex justify-center'>
+				<main className=' flex flex-col sm:flex-row w-[1200px] justify-center'>
+					<NavBar />
+					<div className='w-[100vw] sm:w-[450px] md:w-[600px] xl:w-[700px] border-r-2 border-l-2 border-gray-200 flex flex-col'>
+						{children}
+					</div>
+					<div className='hidden xd:flex xd:flex-col xd:w-[300px] xl:w-[350px]'>
+						<SmUserCard />
+					</div>
+				</main>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,11 @@ import SmUserCard from '@/Components/UserCard/SmUserCard';
 
 export default function Home() {
 	return (
-		<main className=' flex flex-col sm:flex-row w-[1200px] justify-center'>
-			<NavBar />
-			<div className='w-[100vw] sm:w-[450px] md:w-[600px] xl:w-[700px] border-r-2 border-l-2 border-gray-200 flex flex-col'>
-				<SmUserCard />
-				<SmUserCard />
-				<SmUserCard />
-				<SmUserCard />
-			</div>
-			<div className='hidden xd:flex xd:flex-col xd:w-[300px] xl:w-[350px]'>
-				<SmUserCard />
-			</div>
+		<main>
+			<SmUserCard />
+			<SmUserCard />
+			<SmUserCard />
+			<SmUserCard />
 		</main>
 	);
 }
