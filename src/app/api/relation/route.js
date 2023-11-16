5a7c5b0ac135fb5103e63ab29d5e330bb9b60df5
abdulmateenzwl <abdulmateenzwl@gmail.com>
2023-11-16 20:18:04 +0500

    Add POST routes for creating relations and users Created POST API endpoints for creating relation & signing user

--- src/app/api/relation/route.js
+++ src/app/api/relation/route.js
@@ -5,5 +5,11 @@ import { connectionStr } from '../../../lib/db';
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
+	const payload = await request.json();
 
+	await mongoose.connect(connectionStr);
+	let relation = new Relation(payload);
+	await relation.save();
+
+	return NextResponse.json({ message: 'Hello from API route', relation });
 }
--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -5,6 +5,12 @@ import { connectionStr } from '../../../lib/db';
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
+	const payload = await request.json();
 
+	await mongoose.connect(connectionStr);
+	let user = new User(payload);
+	await user.save();
+
+	return NextResponse.json({ message: 'Hello from API route', user });
 
 }
