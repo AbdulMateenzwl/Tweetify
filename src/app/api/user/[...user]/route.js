43a00156380bfb2e5e10b5b7029da2a803a53330
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2023-11-22 15:20:58 +0500

    Fix user route and add tweet route for each user

--- src/app/api/user/[...user]/route.js
+++ src/app/api/user/[...user]/route.js
@@ -6,16 +6,17 @@ import { NextResponse } from 'next/server';
 
 // Get User with username and password
 export async function GET(request, content) {
-	try{
+	try {
 		await mongoose.connect(connectionStr);
-		let findUser = await User.findOne({ username: content.params.user[0],passwordHash: content.params.user[1] });
-		if(findUser == null){
-			return NextResponse.json({ findUser },{status:404});
+		let findUser = await User.findOne({
+			username: content.params.user[0],
+			passwordHash: content.params.user[1],
+		});
+		if (findUser == null) {
+			return NextResponse.json({ findUser }, { status: 404 });
 		}
-		return NextResponse.json({ findUser },{status:200});
-		// return NextResponse.json({status:200});
-	}
-	catch{
+		return NextResponse.json({ findUser }, { status: 200 });
+	} catch {
 		internalServerError();
 	}
 }
@@ -43,6 +44,36 @@ export async function PUT(request, content) {
 		await user.save();
 
 		return NextResponse.json({ user }, { status: 200 });
+	} catch {
+		internalServerError();
+	}
+}
+
+export async function DELETE(request, content) {
+	try {
+		await mongoose.connect(connectionStr);
+
+		// Extract user ID from the request parameters or body
+		const userId = content.params.user[0];
+
+		// Find the user by ID
+		let user = await User.findById(userId);
+
+		// Check if the user exists
+		if (!user) {
+			return notFoundError();
+		}
+
+		// Set isDeleted to true instead of physically deleting the user
+		user.isDeleted = true;
+
+		// Save the updated user
+		await user.save();
+
+		return NextResponse.json(
+			{ message: 'User marked as deleted successfully' },
+			{ status: 200 }
+		);
 	} catch {
 		internalServerError();
 	}
---
+++ src/app/api/user/tweet/[...tweet]/route.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import { Tweet } from '../../../../../lib/model/tweet';
+import { internalServerError, notFoundError } from '@/lib/errors/server';
+import { connectionStr } from '../../../../../lib/db';
+import { NextResponse } from 'next/server';
+
+export async function GET(request, content) {
+	try {
+		await mongoose.connect(connectionStr);
+
+		// Extract user ID from the request parameters or body
+		const user_Id = await content.params.tweet[0];
+		// // Retrieve tweets of the user where isDeleted is false
+		const tweets = await Tweet.find({ userId: user_Id, isDeleted: false });
+
+		return NextResponse.json({ tweets }, { status: 200 });
+
+		return NextResponse.json({ status: 200 });
+	} catch {
+		// internalServerError();
+	}
+}
+
+export async function DELETE(request, content) {
+	try {
+		await mongoose.connect(connectionStr);
+
+		// Extract user ID from the request parameters or body
+		const user_Id = await content.params.tweet[0];
+
+		// Update all tweets of the user to mark them as deleted
+		const result = await Tweet.updateMany(
+			{ userId: user_Id },
+			{
+				$set: {
+					isDeleted: true,
+					updatedAt: new Date().toISOString(),
+				},
+			}
+		);
+
+		// Check if any tweets were updated
+		return NextResponse.json(
+			{ message: 'All tweets of the user marked as deleted successfully' },
+			{ status: 200 }
+		);
+	} catch {
+		internalServerError();
+	}
+}
